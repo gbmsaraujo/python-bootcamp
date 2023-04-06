05bc997117a3ef045bdda0b84fe0f4a65c8ab539
Gabriel Araujo <gbmsaraujo@gmail.com>
2023-04-06 15:45:40 -0300

    Create database.py

---
+++ aulas/api-course/01-fastapi/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/fastapi"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# establishes all conversations with the database and represents a “holding zone”
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Criado para extender todas as models que iremos criar
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
