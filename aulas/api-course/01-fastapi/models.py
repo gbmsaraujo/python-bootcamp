99b14eb1362ba8f70c55b9a496d48e2c8fb8b0d2
Gabriel Araujo <gbmsaraujo@gmail.com>
2023-04-11 18:04:39 -0300

    Update models.py

--- aulas/api-course/01-fastapi/models.py
+++ aulas/api-course/01-fastapi/models.py
@@ -1,7 +1,8 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
+
 
 class Post(Base):
     __tablename__ = "posts"
@@ -9,5 +10,20 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, server_default='True', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+    owner_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+
+
+class User(Base):
+    __tablename__ = "users"
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
